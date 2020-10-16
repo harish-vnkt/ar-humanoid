e2078bffe1dcacc7154e8244f4d4a57dea5a1125
Harish Venkataraman <harishvenkat238@gmail.com>
2020-10-15 23:17:29 -0500

    Scripts for generating red balls on mouse click

---
+++ Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject redBall;
+    public int number;
+    float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            for (int i = 0; i < number; i++) {
+                float angle = i * Mathf.PI * 2 / number;
+                float x = Mathf.Cos(angle) * Random.Range(0f, radius);
+                float z = Mathf.Sin(angle) * Random.Range(0f, radius);
+                Vector3 pos = transform.position + new Vector3(x, 0, z);
+                float angleDegrees = -angle * Mathf.Rad2Deg;
+                Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+                Instantiate(redBall, pos, rot);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RedBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBall : MonoBehaviour
+{   
+    public float timeToLive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timeToLive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
