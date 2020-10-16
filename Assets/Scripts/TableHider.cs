8b4d569957988eb03548311ac9e833cb8d93d087
Harish Venkataraman <harishvenkat238@gmail.com>
2020-10-15 23:17:59 -0500

    Make table top cube invisible

---
+++ Assets/Scripts/TableHider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableHider : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (meshRenderer.enabled) {
+            meshRenderer.enabled = false;
+        }
+    }
+}
